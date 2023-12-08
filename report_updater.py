a4a4302ebdbf4b707ee6141659c82dc34c7ca00b
Davinchiam1 <ruslananushko2113@gmail.com>
2023-12-08 09:51:29 +0300

    add comments

--- report_updater.py
+++ report_updater.py
@@ -5,10 +5,26 @@ import time
 import json
 
 
-def load_sales_and_traf(id='',file=False,timedelta=2, re_update_rate=30):
+def load_sales_and_traf(id='', file=False, timedelta=2, re_update_rate=1):
+    """
+        Function for loading last sales and traffic data by GET_SALES_AND_TRAFFIC_REPORT.
+
+        Args:
+            id(str): id of already created report, if empty, new report will ber requested. Default empty
+            file(bool): mark to load data from previously loaded file (temp.txt in project folder). Default False.
+            timedelta(int): counter to set date for creating of a report. reports are created on a date 'timedelta' days
+            away from the current one. Defalut 2 days, and can't be less
+            re_update_rate(int): countr to set amount of re-updating reports(days). If specified, reload data from
+            'timedelta' to 'timedelta+re_update_rate' days from the current one. Default 1
+
+        Returns:
+            None
+
+        """
 
     for i in range(re_update_rate):
-        date = datetime.datetime.now().date() - datetime.timedelta(timedelta+i)
+        print(i + 1)
+        date = datetime.datetime.now().date() - datetime.timedelta(timedelta + i)
 
         date = date.strftime("%d.%m.%Y")
         salesandtraf = Report_loader()
@@ -34,6 +50,4 @@ def load_sales_and_traf(id='',file=False,timedelta=2, re_update_rate=30):
             update_report_table(reports_names=['sales_and_traf', 'sales_and_traf_by_asin'], df_list=doc_content)
 
 
-
-load_sales_and_traf(id='',timedelta=2)
-
+load_sales_and_traf(id='', timedelta=2,re_update_rate=30)
