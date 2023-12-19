3dac9608d1889b1e52bc7104fc4c8a3ba3fd1cda
Davinchiam1 <ruslananushko2113@gmail.com>
2023-12-19 10:35:43 +0300

    Changes in LICENSE.txt and add additional files

---
+++ credentials.py
@@ -0,0 +1,8 @@
+credentials=dict(
+    refresh_token='your_refresh_token',
+    lwa_app_id='your_app_id',
+    lwa_client_secret='your_client_secret')
+proxy = {
+    'http': 'your_http_proxy',
+    'https': 'your_https_proxy'
+}
